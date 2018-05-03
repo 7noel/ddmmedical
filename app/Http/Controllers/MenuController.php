f2e4f763407ee19821600df1ad48e43af3f6e160
7noel <noel.logan@gmail.com>
2018-05-03 14:58:41 -0500

    print_order

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -79,7 +79,7 @@ class MenuController extends Controller
                 ['name' => 'Medios de Pago', 'route' => 'payment_conditions.index' ],
             ],
             'Ventas'=>[
-                ['name' => 'Notas de Pedido', 'route' => 'orders.index' ],
+                ['name' => 'Cotizaciones', 'route' => 'orders.index' ],
                 ['name' => 'Validar Notas de Pedido', 'url' => '#' ],
                 ['name' => 'Facturación', 'url' => '#' ],
             ],
